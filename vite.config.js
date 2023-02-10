d3eaeaefe3342a81c2c8c1197e82bdc7827ab398
Mateus Neiva <mateusneiva.dev@gmail.com>
2023-02-10 20:51:53 -0300

    chore(deps): remove vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
 });
